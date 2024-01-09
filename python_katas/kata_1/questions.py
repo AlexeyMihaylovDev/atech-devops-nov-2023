8dc4060e30f7c49275bd39ee5f8a08f333b0cdbb
Abedgz <gz.abed@gmail.com>
2024-01-09 10:08:31 +0200

    new sloves

--- python_katas/kata_1/questions.py
+++ python_katas/kata_1/questions.py
@@ -188,23 +188,8 @@ def print_dict_as_table(some_dict):
 
 
 def merge_dicts(dict1, dict2):
-    """
-    1 Kata
-
-    This functions merges dict2's keys and values into dict1, and returns dict1
-
-    e.g.
-    dict1 = {'a': 1}
-    dict2 = {'b': 2}
-
-    The results will by
-    dict1 = {'a': 1, 'b': 2}
-
-    :param dict1:
-    :param dict2:
-    :return:
-    """
-    return dict1
+    
+    return {**dict1 , **dict2}
 
 
 def seven_boom(n):
@@ -324,11 +309,11 @@ if __name__ == '__main__':
     #     "Rachel": 95
     # }))
 
-    # print('\nmerge_dicts:\n--------------------')
-    # print(merge_dicts({'a': 1}, {'b': 2}))
+    print('\nmerge_dicts:\n--------------------')
+    print(merge_dicts({'a': 1}, {'b': 2}))
 
-    print('\nseven_boom:\n--------------------')
-    print(seven_boom(14))
+    #print('\nseven_boom:\n--------------------')
+    #print(seven_boom(14))
 
     # print('\ncaesar_cipher:\n--------------------')
     # print(caesar_cipher('Fly Me To The Moon'))
